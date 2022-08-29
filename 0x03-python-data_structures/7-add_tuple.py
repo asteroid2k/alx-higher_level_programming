98d78df8e30bf25fb280628fe3efedcf82eca6bc
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-08-29 12:03:39 +0000

    7-dd_tupes

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1, *a2 = tuple_a
+    b1, *b2 = tuple_b
+    a2 = a2 if a2 else 0
+    b2 = b2 if b2 else 0
+    return (a1 + b1, a2 + b2)
