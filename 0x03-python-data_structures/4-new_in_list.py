7b49a25daf4f5665d51baab974c55471ab6a4cb5
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-08-29 11:31:44 +0000

    4-new

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
     copy = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return copy
-    copy.insert(idx, element)
+    copy[idx] = element
     return copy
