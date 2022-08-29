65cc83810c4a0268f84f0595cf2fcdec82443997
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-08-29 11:52:22 +0000

    6-matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
+        for idx, col in enumerate(row):
+            end_c = " " if idx != len(row) - 1 else ""
+            print("{:d}".format(col), end=end_c)
         print()
