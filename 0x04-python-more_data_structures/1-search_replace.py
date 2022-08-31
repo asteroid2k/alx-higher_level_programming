3a08516d6f97a7c9d2a11dde514b9450241c1f08
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-08-31 08:01:32 +0000

    1-search-n-replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,10 +6,3 @@ def search_replace(my_list, search, replace):
         val = val if val != search else replace
         new_list.append(val)
     return new_list
-
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
