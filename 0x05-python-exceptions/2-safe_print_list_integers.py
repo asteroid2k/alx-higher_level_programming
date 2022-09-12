fd64733d5ed955dd66092d95d15b8d8ebe3f1d1d
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-12 18:27:43 +0000

    4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     elements = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print(f"{my_list[i]:d}", end="")
             elements += 1
         except:
             pass
