f8aff5f877cc2946990c0d20b400e44f5c64db51
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-12 18:35:02 +0000

    1 fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,26 @@
 #!usr/bin/python3
 
 def safe_print_integer(value):
+    if value is None:
+        return False
     try:
         print("{:d}".format(value))
         return True
     except:
         return False
+
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
