7c72d4d89806247a8fb661322e1be868caedc371
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-12 19:46:08 +0000

    100 fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,28 @@ import sys
 
 
 def safe_print_integer_err(value):
+    ex = ""
     try:
         print("{:d}".format(value))
         return True
     except Exception as e:
+        ex = e
         return False
     finally:
-        sys.stderr.write("Exception: {}".format(e))
+        sys.stderr.write("Exception: {}".format(ex))
+
+
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
