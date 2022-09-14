913e250a4a222053d4c459648526844a0b5b4e63
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-14 07:36:09 +0000

    comments

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+'''Module to define a Square class'''
+
+
 class Square:
+    '''Represents a Square class'''
+
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
