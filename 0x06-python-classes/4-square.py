22a2df5f0c966c2b31ecf6b8a10965377a5cb78b
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-14 07:39:29 +0000

    4 fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,8 +13,13 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
     def size(self):
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
 
     def area(self):
         return
