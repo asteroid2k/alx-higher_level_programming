e138d0624798ff59ddb98635f15de0fbe6744c76
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-14 08:05:11 +0000

    6 fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,8 @@ class Square:
         for v in range(self.__position[1]):
             print()
         for i in range(self.__size):
-            print(" "*self.__position[0], "#" * self.__size)
+            print(" "*self.__position[0], end="")
+            print("#" * self.__size)
 
     @property
     def position(self):
