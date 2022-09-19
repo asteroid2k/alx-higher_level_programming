d03888630448f705ebabf1a36ba6501cdd44174f
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-19 12:34:29 +0000

    1 rect fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,6 +35,6 @@ class Rectangle:
         """This is the setter of the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__height = value
