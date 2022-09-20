42a2f8bce007a1a1a2dff71c20f0b2d1a11ba843
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-20 07:59:31 +0000

    1 rect fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """This is the getter of the width"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -33,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """This is the setter of the height"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
