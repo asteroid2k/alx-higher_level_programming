c871ddfc09b86f19835c34eb8637cb832cc518da
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-27 08:01:49 +0000

    2 is same

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module for is_kind_of_class function"""
+
 
 def is_same_class(obj, a_class):
-    """Returns True if the object is exactly an instance of the specified class"""
+    """Returns True if the object is exactly an
+     instance of the specified class"""
     return type(obj) == a_class
