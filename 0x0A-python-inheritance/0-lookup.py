b6232bbd4e3d9f2f83a111d1503436e86e6f1d59
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-27 07:40:41 +0000

    0 fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module to lookup all methods and attribute of an object"""
+
 
 def lookup(obj):
     """function that returns the list of available attributes and methods of an object"""
