bb1c5ed2841b4a5135722b53457f085dc578580d
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-28 09:22:16 +0000

    inherit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """Module for Rectangle class"""
 
 
-class Rectangle():
+class Rectangle(BaseGeometry):
     """Rectangle class"""
 
     def __init__(self, width, height):
