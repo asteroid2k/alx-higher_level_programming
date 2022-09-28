e9b3cf153960bfb4cd6454291facac4dd28ec7f6
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-28 13:03:25 +0000

    8

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,6 +7,7 @@ class BaseGeometry():
     """BaseGeometry class"""
 
     def area(self):
+        """area method"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
