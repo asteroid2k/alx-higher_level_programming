b1e989ff6409bb34e9645cfb57401061e96b98cf
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-28 09:26:01 +0000

    inherit fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """Module for Rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -8,17 +9,8 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """__init__ method"""
+        super.__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer_validator method"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
