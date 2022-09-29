2ac282bdafbab997a61272564f9f8b25c9b72c27
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-29 07:47:44 +0000

    save json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Module save JSON representation of an object to file"""
-
-
 import json
 
 
-def to_json_string(my_obj: str, filename: str):
+def save_to_json_file(my_obj: str, filename: str):
     """Function saves JSON representation of an object"""
     with open(filename, mode="w", encoding="utf-8") as f:
         json.dump(my_obj, f)
