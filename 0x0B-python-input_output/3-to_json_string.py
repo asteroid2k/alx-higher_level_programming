6af5bb8ba25b868b2f2861b7710e0bb2d742480f
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-29 07:30:06 +0000

    to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""Module returns JSON representation of an object"""
 import json
 
-"""Module returns JSON representation of an object"""
 
-
-def to_json_string(my_obj):
+def to_json_string(my_obj) -> str:
     """Function returns JSON representation of an object"""
     return json.dumps(my_obj)
