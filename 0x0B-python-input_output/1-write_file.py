d2458b32b6bba161cd7768176c61bac64440bc64
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-09-29 07:08:28 +0000

    1write

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for write_file method.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes to a text file (UTF8).
+    Args:
+        filename (str): The name of the file to read.
+        text (str): text to write to file.
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
+
+
+write_file("aa.txt", "Sexy")
